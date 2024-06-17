694067928088597daebf86003a4fddd64214e82f
KubaChala <jakubchala01@gmail.com>
2024-06-17 18:19:31 +0200

    Utworzenie klasy

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Poczatek programu");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int number = sc.nextInt();
+        System.out.println("Your number is: " + number);
 
-
+        sc.close();
+        System.out.println("Koniec programu");
+        System.out.println("Pocztaek klasy");
+        Welcome welcome = new Welcome();
+        welcome.przywiatnie("Kuba");
+        System.out.println("Koniec klasy");
     }
 }
---
+++ src/Welcome.java
@@ -0,0 +1,5 @@
+public class Welcome {
+    public void przywiatnie(String imie){
+        System.out.println("Witaj " + imie);
+    }
+}
